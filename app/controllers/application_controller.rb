cfea96b4ba22e7a7b20e0e0d4a32b18869002dd1
Christopher Arnold <chrisarnold2010@gmail.com>
2018-08-10 16:37:34 -0500

    almost done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,55 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+#INITIATE DATABASE BY CREATING A MIGRATION. ALSO NEED TO CREATE A CLASS TO MOVE THE MIGRATION INTO.
+#START REST METHOD
 
+ #CREATE
+
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
+
+
+ #READ
+
+  get '/posts' do 
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  #UPDATE
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+  	erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name
+    @post.content
+    @post.save
+    erb :show
+  end
+
+  #DELETE
+
+  get '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    redirect to '/posts'
+  end
+
+
 end
